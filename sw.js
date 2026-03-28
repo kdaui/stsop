6d052ee7076785a074141ca9acc5c89a7301ffda
kdaui <bilal.q2020@gmail.com>
2026-03-28 19:11:22 +0000

    Add PWA support with manifest and service worker

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'stsop-v1';
+const urlsToCache = [
+  '/stsop/',
+  '/stsop/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
